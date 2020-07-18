1f259086144d11c0828df97d05e751f194e67ad8
Almaz Shagiev <exaggeratethismotion@gmail.com>
2020-07-18 15:43:34 +0500

    initial commit

---
+++ STRING!.cpp
@@ -0,0 +1,46 @@
+﻿
+#include <iostream>
+using namespace std;
+
+class String
+{
+private:
+    int size;
+    char str[255];
+public:
+    String(char str[])
+    {
+        size = strlen(str);
+        strcpy_s(this->str, str);
+    }
+    String(char str[], int k)
+    {
+        size = k;
+        for (int i = 0; i < k; i++)
+        {
+            this->str[i] = str[0];
+        }
+        this->str[k] = '\0';
+    }
+    void Print()
+    {
+        cout << str << endl;
+    }
+    ~String()
+    {
+        delete[] str;
+    }
+};
+
+int main()
+{
+    char *string1;
+    cin >> string1;
+    String st1(string1);
+    st1.Print();
+    char string2[1];
+    cin >> string2;
+    String st2(string2, 5);
+    st2.Print();
+    return 0;
+}
